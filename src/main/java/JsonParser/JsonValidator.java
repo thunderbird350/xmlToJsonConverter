4557083be8b260656ede6c06f372cdc0b52e8b46
thunderbird350 <mdazaruddin53@gmail.com>
2018-03-09 10:30:54 +0530

    many changes

--- src/main/java/JsonParser/JsonValidator.java
+++ src/main/java/JsonParser/JsonValidator.java
@@ -1,44 +1,53 @@
 
 package JsonParser;
 
-import org.everit.json.schema.Schema;
-import org.everit.json.schema.ValidationException;
-import org.everit.json.schema.loader.SchemaLoader;
-import org.json.JSONObject;
+import java.io.IOException;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.github.fge.jackson.JsonLoader;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingMessage;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
+import com.github.fge.jsonschema.main.JsonSchema;
+import com.github.fge.jsonschema.main.JsonSchemaFactory;
+import com.google.common.base.StandardSystemProperty;
+import com.google.common.collect.Lists;
 
 public class JsonValidator {
 	
 	
-	public static String jsonValidator(String jsonpayLoad,String jsonschema)
+	public static String jsonValidator(String jsonpayLoad,String jsonschema) throws IOException, ProcessingException
 	{
 		
-		try{
+		final String LS = StandardSystemProperty.LINE_SEPARATOR.value();
+			JsonNode jsonPayJsonNode = JsonLoader.fromString(jsonpayLoad);
+			 JsonNode schemaNode = JsonLoader.fromString(jsonschema);
+			 JsonSchemaFactory factory=JsonSchemaFactory.byDefault();
+			 JsonSchema jSchema = factory.getJsonSchema(schemaNode);
+			 ProcessingReport report = jSchema.validate(jsonPayJsonNode);
 			
-			JSONObject jsonPayLoad = new JSONObject(jsonpayLoad);
-			JSONObject jsonSchema = new JSONObject(jsonschema);
 			
-			Schema schemaT = SchemaLoader.load(jsonSchema);
-			
-			schemaT.validate(jsonPayLoad);
-						
-			
-		}catch(ValidationException e)
-		{
-			
-			System.out.println("I am in excepption");
-			return e.getMessage();
-		}
-		
+			 if (!report.isSuccess()) {
+
+					StringBuilder builder = new StringBuilder();
+					builder.append("Could not validate JSON against schema");
+				//	builder.append(getDescriptionAppendable());
+					builder.append(":");
+				//	builder.append(LS);
+					List<ProcessingMessage> messages = Lists.newArrayList(report);
+					for (int i = 0; i < messages.size(); i++) {
+						builder.append("- ");
+						builder.append(messages.get(i).getMessage());
+						//builder.append(i == (messages.size() - 1) ?  : LS);
+					}
+					return builder.toString();
+				}	
 
 		return "Valid";
 	}
 	
-	public static void main(String[] args) {
 
-		String r=jsonValidator("","");
-		
-		System.out.println(r);
-	}
 	
 	
 
